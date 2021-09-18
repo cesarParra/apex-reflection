9959aa1dc932a48595043be622c421492cbe125c
Cesar Parra <cesar.parra@gmail.com>
2021-09-18 18:49:08 -0400

    Additional unit tests for the ClassModel object

--- test/class_model_test.dart
+++ test/class_model_test.dart
@@ -43,6 +43,54 @@ void main() {
       expect(classModel.implementedInterfaces.length, 2);
     });
 
+    test('Can determine if is private', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['private']);
+      expect(classModel.isPrivate, isTrue);
+    });
+
+    test('Can determine if is public', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['public']);
+      expect(classModel.isPublic, isTrue);
+    });
+
+    test('Can determine if is global', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['global']);
+      expect(classModel.isGlobal, isTrue);
+    });
+
+    test('Can determine if is protected', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['protected']);
+      expect(classModel.isProtected, isTrue);
+    });
+
+    test('Can determine if is virtual', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['public', 'virtual']);
+      expect(classModel.isVirtual, isTrue);
+    });
+
+    test('Can determine if is NamespaceAccessible', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['namespaceaccessible']);
+      expect(classModel.isNamespaceAccessible, isTrue);
+    });
+
+    test('Can determine if is test', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['istest']);
+      expect(classModel.isTest, isTrue);
+    });
+
+    test('Can determine if is deprecated', () {
+      final classModel =
+          ClassModel(name: 'AnyName', accessModifiers: ['deprecated']);
+      expect(classModel.isDeprecated, isTrue);
+    });
+
     // TODO: can have a constructor
     // TODO: can have a list of properties
     // TODO: can have a list of methods

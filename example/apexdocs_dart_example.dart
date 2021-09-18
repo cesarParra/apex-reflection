e95b25d774162a10516c866ec566a55c5b3ca699
Cesar Parra <cesar.parra@gmail.com>
2021-09-18 13:45:19 -0400

    Initial commit - Project creation

---
+++ example/apexdocs_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:apexdocs_dart/apexdocs_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/apexdocs_dart.dart
@@ -0,0 +1,5 @@
+library apexdocs_dart;
+
+export 'src/apexdocs_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

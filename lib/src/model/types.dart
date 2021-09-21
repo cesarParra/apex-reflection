104558e76324b8b163b0cf99e801c78028d8901c
Cesar Parra <cesar.parra@gmail.com>
2021-09-20 21:11:49 -0400

    Types serialization

--- lib/src/model/types.dart
+++ lib/src/model/types.dart
@@ -28,8 +28,8 @@ mixin MethodsAwareness {
 }
 
 class ClassModel extends Type with MethodsAwareness {
-  final String? extendedClass;
-  final List<String> implementedInterfaces;
+  late final String? extendedClass;
+  late final List<String> implementedInterfaces;
 
   List<Property> properties = [];
   List<Field> fields = [];
@@ -44,6 +44,56 @@ class ClassModel extends Type with MethodsAwareness {
       this.extendedClass,
       this.implementedInterfaces = const []})
       : super(name: name, accessModifiers: accessModifiers);
+
+  ClassModel.fromJson(Map<String, dynamic> json)
+      : super(
+            name: json['name'],
+            accessModifiers: json['access_modifiers'].cast<String>()) {
+    extendedClass = json['extended_class'];
+    implementedInterfaces = json['implemented_interfaces'].cast<String>();
+
+    // Constructor deserialization
+    List<dynamic> encodedConstructors = json['constructors'];
+    constructors = encodedConstructors
+        .map((e) => Constructor.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Property deserialization
+    List<dynamic> encodedProperties = json['properties'];
+    properties = encodedProperties
+        .map((e) => Property.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Field deserialization
+    List<dynamic> encodedFields = json['fields'];
+    fields = encodedFields
+        .map((e) => Field.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Method deserialization
+    List<dynamic> encodedMethods = json['methods'];
+    methods = encodedMethods
+        .map((e) => Method.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Interface deserialization
+    List<dynamic> encodedInterfaces = json['interfaces'];
+    interfaces = encodedInterfaces
+        .map((e) => InterfaceModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Enum deserialization
+    List<dynamic> encodedEnums = json['enums'];
+    enums = encodedEnums
+        .map((e) => EnumModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    // Class deserialization
+    List<dynamic> encodedClasses = json['classes'];
+    classes = encodedClasses
+        .map((e) => ClassModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
 
   @override
   bool isClass() {
@@ -73,10 +123,24 @@ class ClassModel extends Type with MethodsAwareness {
   void addClass(ClassModel innerClass) {
     classes.add(innerClass);
   }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'extended_class': extendedClass,
+        'implemented_interfaces': implementedInterfaces,
+        'access_modifiers': accessModifiers,
+        'fields': fields.map((e) => e.toJson()).toList(),
+        'constructors': constructors.map((e) => e.toJson()).toList(),
+        'properties': properties.map((e) => e.toJson()).toList(),
+        'methods': methods.map((e) => e.toJson()).toList(),
+        'interfaces': interfaces.map((e) => e.toJson()).toList(),
+        'enums': enums.map((e) => e.toJson()).toList(),
+        'classes': classes.map((e) => e.toJson()).toList(),
+      };
 }
 
 class InterfaceModel extends Type with MethodsAwareness {
-  final List<String> extendedInterfaces;
+  late final List<String> extendedInterfaces;
 
   InterfaceModel(
       {required String name,
@@ -84,10 +148,28 @@ class InterfaceModel extends Type with MethodsAwareness {
       this.extendedInterfaces = const []})
       : super(name: name, accessModifiers: accessModifiers);
 
+  InterfaceModel.fromJson(Map<String, dynamic> json)
+      : super(
+            name: json['name'],
+            accessModifiers: json['access_modifiers'].cast<String>()) {
+    extendedInterfaces = json['extended_interfaces'].cast<String>();
+    List<dynamic> encodedMethods = json['methods'];
+    methods = encodedMethods
+        .map((e) => Method.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+
   @override
   bool isInterface() {
     return true;
   }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'extended_interfaces': extendedInterfaces,
+        'access_modifiers': accessModifiers,
+        'methods': methods.map((e) => e.toJson()).toList()
+      };
 }
 
 class EnumModel extends Type {
@@ -98,4 +180,12 @@ class EnumModel extends Type {
   bool isEnum() {
     return true;
   }
+
+  EnumModel.fromJson(Map<String, dynamic> json)
+      : super(
+            name: json['name'],
+            accessModifiers: json['access_modifiers'].cast<String>());
+
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'access_modifiers': accessModifiers};
 }
---
+++ test/types_serialization_test.dart
@@ -0,0 +1,192 @@
+import 'dart:convert';
+
+import 'package:apexdocs_dart/src/model/members.dart';
+import 'package:apexdocs_dart/src/model/types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Class serialization', () {
+    test('Classes can be serialized', () {
+      var classModel = ClassModel(
+          name: 'ClassName',
+          extendedClass: 'ParentClass',
+          implementedInterfaces: ['Interface1', 'Interface2'],
+          accessModifiers: ['namespaceaccessible', 'public']);
+      classModel.addInterface(InterfaceModel(name: 'ChildInterface'));
+      classModel.addEnum(EnumModel(name: 'ChildEnum'));
+      classModel.addClass(ClassModel(name: 'ChildClass'));
+      classModel.addProperty(Property(name: 'Prop1', type: 'String'));
+      classModel.addConstructor(
+          Constructor(accessModifiers: ['namespaceaccessible', 'public']));
+      classModel.addMethod(Method(name: 'doSomething'));
+      classModel.addField(Field(name: 'var1', type: 'String'));
+
+      String encodedClass = jsonEncode(classModel);
+      expect(encodedClass, isNotNull);
+    });
+
+    test('Classes can be deserialized', () {
+      final classAsJson = '''
+      {
+        "name": "ClassName",
+        "extended_class": "ParentClass",
+        "implemented_interfaces": [
+          "Interface1",
+          "Interface2"
+        ],
+        "access_modifiers": [
+          "namespaceaccessible",
+          "public"
+        ],
+        "fields": [
+          {
+            "name": "var1",
+            "type": "String",
+            "access_modifiers": []
+          }
+        ],
+        "constructors": [
+          {
+            "access_modifiers": [
+              "namespaceaccessible",
+              "public"
+            ],
+            "parameters": []
+          }
+        ],
+        "properties": [
+          {
+            "name": "Prop1",
+            "type": "String",
+            "access_modifiers": []
+          }
+        ],
+        "methods": [
+          {
+            "name": "doSomething",
+            "type": "void",
+            "access_modifiers": [],
+            "parameters": []
+          }
+        ],
+        "interfaces": [
+          {
+            "name": "ChildInterface",
+            "extended_interfaces": [],
+            "access_modifiers": [],
+            "methods": []
+          }
+        ],
+        "enums": [
+          {
+            "name": "ChildEnum",
+            "access_modifiers": []
+          }
+        ],
+        "classes": [
+          {
+            "name": "ChildClass",
+            "implemented_interfaces": [],
+            "access_modifiers": [],
+            "fields": [],
+            "constructors": [],
+            "properties": [],
+            "methods": [],
+            "interfaces": [],
+            "enums": [],
+            "classes": []
+          }
+        ]
+      }
+      ''';
+
+      final classModel = ClassModel.fromJson(jsonDecode(classAsJson));
+      expect(classModel.name, equals('ClassName'));
+      expect(classModel.extendedClass, equals('ParentClass'));
+      expect(classModel.implementedInterfaces,
+          equals(['Interface1', 'Interface2']));
+      expect(classModel.accessModifiers,
+          equals(['namespaceaccessible', 'public']));
+      expect(classModel.fields, hasLength(1));
+      expect(classModel.properties, hasLength(1));
+      expect(classModel.constructors, hasLength(1));
+      expect(classModel.methods, hasLength(1));
+      expect(classModel.classes, hasLength(1));
+      expect(classModel.enums, hasLength(1));
+      expect(classModel.interfaces, hasLength(1));
+    });
+  });
+
+  group('Interface serialization', () {
+    test('Interfaces can be serialized', () {
+      var interfaceModel = InterfaceModel(
+          name: 'InterfaceName',
+          extendedInterfaces: ['Interface1', 'Interface2'],
+          accessModifiers: ['namespaceaccessible', 'public']);
+      interfaceModel.addMethod(Method(name: 'doSomething'));
+
+      String encodedClass = jsonEncode(interfaceModel);
+      expect(encodedClass, isNotNull);
+    });
+
+    test('Interfaces can be deserialized', () {
+      final interfaceAsJson = '''
+      {
+        "name": "InterfaceName",
+        "extended_interfaces": [
+          "Interface1",
+          "Interface2"
+        ],
+        "access_modifiers": [
+          "namespaceaccessible",
+          "public"
+        ],
+        "methods": [
+          {
+            "name": "doSomething",
+            "type": "void",
+            "access_modifiers": [],
+            "parameters": []
+          }
+        ]
+      }
+      ''';
+
+      final interfaceModel =
+          InterfaceModel.fromJson(jsonDecode(interfaceAsJson));
+      expect(interfaceModel.name, equals('InterfaceName'));
+      expect(interfaceModel.accessModifiers,
+          equals(['namespaceaccessible', 'public']));
+      expect(interfaceModel.methods, hasLength(1));
+      expect(interfaceModel.methods.first.name, 'doSomething');
+    });
+  });
+
+  group('Enum serialization', () {
+    test('Enums can be serialized', () {
+      var enumModel = EnumModel(
+          name: 'EnumName', accessModifiers: ['namespaceaccessible', 'public']);
+
+      String encodedEnum = jsonEncode(enumModel);
+      print(encodedEnum);
+      expect(encodedEnum, isNotNull);
+    });
+
+    test('Fields can be deserialized', () {
+      final enumAsJson = '''
+      {
+        "name": "EnumName",
+        "access_modifiers": [
+          "namespaceaccessible",
+          "public"
+        ]
+      }
+      ''';
+
+      final enumModel = EnumModel.fromJson(jsonDecode(enumAsJson));
+      expect(enumModel.name, equals('EnumName'));
+      expect(
+          enumModel.accessModifiers, equals(['namespaceaccessible', 'public']));
+    });
+  });
+}

e2ed09e407d96485fc5e471c22fac13623308996
Cesar Parra <cesar.parra@gmail.com>
2021-09-22 08:59:30 -0400

    Apexdoc parser and lexer

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ To generate the Antlr4 Apex output run:
 ```
 antlr4 -Dlanguage=Dart lib/src/antlr/grammars/apex/ApexLexer.g4 lib/src/antlr/grammars/apex/ApexParser.g4 -o lib/src/antlr/lib/apex/
 ```
+
+To generate the Antlr4 Apexdoc output run:
+
+```
+antlr4 -Dlanguage=Dart lib/src/antlr/grammars/apexdoc/ApexdocLexer.g4 lib/src/antlr/grammars/apexdoc/ApexdocParser.g4 -o lib/src/antlr/lib/apexdoc/
+```
---
+++ lib/src/antlr/lib/apexdoc/ApexdocParserBaseListener.dart
@@ -0,0 +1,204 @@
+// Generated from lib/src/antlr/grammars/apexdoc/ApexdocParser.g4 by ANTLR 4.9.2
+// ignore_for_file: unused_import, unused_local_variable, prefer_single_quotes
+import 'package:antlr4/antlr4.dart';
+
+import 'ApexdocParser.dart';
+import 'ApexdocParserListener.dart';
+
+
+/// This class provides an empty implementation of [ApexdocParserListener],
+/// which can be extended to create a listener which only needs to handle
+/// a subset of the available methods.
+class ApexdocParserBaseListener implements ApexdocParserListener {
+  /// The default implementation does nothing.
+  @override
+  void enterDocumentation(DocumentationContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDocumentation(DocumentationContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDocumentationContent(DocumentationContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDocumentationContent(DocumentationContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterSkipWhitespace(SkipWhitespaceContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitSkipWhitespace(SkipWhitespaceContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescription(DescriptionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescription(DescriptionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionLine(DescriptionLineContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionLine(DescriptionLineContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionLineStart(DescriptionLineStartContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionLineStart(DescriptionLineStartContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionLineNoSpaceNoAt(DescriptionLineNoSpaceNoAtContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionLineNoSpaceNoAt(DescriptionLineNoSpaceNoAtContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionLineElement(DescriptionLineElementContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionLineElement(DescriptionLineElementContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionLineText(DescriptionLineTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionLineText(DescriptionLineTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterDescriptionNewline(DescriptionNewlineContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitDescriptionNewline(DescriptionNewlineContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterTagSection(TagSectionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitTagSection(TagSectionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBlockTag(BlockTagContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBlockTag(BlockTagContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBlockTagName(BlockTagNameContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBlockTagName(BlockTagNameContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBlockTagContent(BlockTagContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBlockTagContent(BlockTagContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBlockTagText(BlockTagTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBlockTagText(BlockTagTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBlockTagTextElement(BlockTagTextElementContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBlockTagTextElement(BlockTagTextElementContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterInlineTag(InlineTagContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitInlineTag(InlineTagContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterInlineTagName(InlineTagNameContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitInlineTagName(InlineTagNameContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterInlineTagContent(InlineTagContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitInlineTagContent(InlineTagContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBraceExpression(BraceExpressionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBraceExpression(BraceExpressionContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBraceContent(BraceContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBraceContent(BraceContentContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterBraceText(BraceTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitBraceText(BraceTextContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void enterEveryRule(ParserRuleContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void exitEveryRule(ParserRuleContext ctx) {}
+
+  /// The default implementation does nothing.
+  @override
+  void visitTerminal(TerminalNode node) {}
+
+  /// The default implementation does nothing.
+  @override
+  void visitErrorNode(ErrorNode node) {}
+}

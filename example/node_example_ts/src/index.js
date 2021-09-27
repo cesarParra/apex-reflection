b13a675ecf50b0951f66256e2e464905ba8c49f0
Cesar Parra <cesar.parra@gmail.com>
2021-09-27 19:03:41 -0400

    Cleaning up node_modules and adding examples

---
+++ example/node_example_ts/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var apex_reflection_1 = require("@cparra/apex-reflection");
+var classBody = "\n/** \n * @description My Class description \n */\n public with sharing class MyClass{}\n";
+console.log((0, apex_reflection_1.reflect)(classBody));

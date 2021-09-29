08fc228709933a82bc6ef6529a690521e8638183
Cesar Parra <cesar.parra@gmail.com>
2021-09-29 08:47:19 -0400

    Moving antlr library directory

--- lib/antlr4-4.9.2/lib/src/ll1_analyzer.dart
+++ lib/antlr4-4.9.2/lib/src/ll1_analyzer.dart
@@ -4,7 +4,7 @@
  * can be found in the LICENSE.txt file in the project root.
  */
 
-import './util/bit_set.dart';
+import 'util/bit_set.dart';
 import 'atn/atn.dart';
 import 'interval_set.dart';
 import 'prediction_context.dart';
